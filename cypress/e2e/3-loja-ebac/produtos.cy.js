012066763c844dc8dfa26f6be6262dbcba56f773
Thiago Cavalcante <thiagoacavalcante_@hotmail.com>
2024-07-31 17:50:02 -0300

    Aula de produtos

---
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -0,0 +1,26 @@
+/// <references types="cypress"/>
+
+describe('Funcionalidade: Produtos', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+    
+    afterEach(() => {
+        cy.screenshot()
+    })
+    
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('.product-block')
+        //.first()
+        //.last()
+        //.eq(2)
+        .contains('Argus All-Weather Tank')
+        .click()
+        .wait(3000)
+
+        cy.get('.product_title').should('exist')
+
+        
+    });
+});
